c30c6e40c5c7b86b6b2098b6ac8d022279051eb6
Evan Martin <martine@danga.com>
2008-08-08 11:11:48 -0700

    parent rev

--- RevParse.hs
+++ RevParse.hs
@@ -6,6 +6,7 @@ import Control.Monad.Error
 import Text.ParserCombinators.Parsec
 
 data Rev = RevHash String
+         | RevParent Int Rev
          | RevSymRef String
          deriving (Eq, Show)
 
@@ -17,8 +18,11 @@ parseRev input =
 
 p_ref :: Parser Rev
 p_ref = do rev <- (p_sha1 <|> p_symref)
-           eof; return rev
+           modrev <- option rev (p_parent rev)
+           eof; return modrev
   where
     p_sha1 = count 40 hexDigit >>= return . RevHash
     -- XXX what chars are allowed in a symref?
     p_symref = many1 (alphaNum <|> char '/') >>= return . RevSymRef
+    -- Mods alter a Rev.
+    p_parent rev = do char '^'; return $ RevParent 1 rev
--- RevParse_test.hs
+++ RevParse_test.hs
@@ -10,12 +10,14 @@ assertParse exp inp =
 testParse label exp inp = TestLabel label $ TestCase (assertParse exp inp)
 
 tests = TestList [
-    sha1, symref
+    sha1, symref, parent
   ]
   where
+    hEAD = RevSymRef "HEAD"
     sha1 = testParse "sha1"
       (RevHash "39cfa1b3e586a092b04cfd81ad9f58844448e845")
       "39cfa1b3e586a092b04cfd81ad9f58844448e845"
     symref = testParse "symref" (RevSymRef "HEAD") "HEAD"
+    parent = testParse "parent" (RevParent 1 hEAD) "HEAD^"
 
 main = runTestTT tests
