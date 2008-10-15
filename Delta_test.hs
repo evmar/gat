0fee27285ee0ae10938fbe3d745ec16440dcfae8
Evan Martin <martine@danga.com>
2008-10-15 09:46:23 -0700

    document Delta_test

--- Delta_test.hs
+++ Delta_test.hs
@@ -5,6 +5,8 @@ import Test.HUnit
 
 import Delta
 
+-- A basic test of the deta code.  Reads testdata/delta.base, applies
+-- testdata/delta as a delta, and verifies the output size is as expected.
 testApply :: Test
 testApply = test $ do
   base <- B.readFile "testdata/delta.base"
