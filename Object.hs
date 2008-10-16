f5a08a7f6ce52e9b682168b2f242d8edabbbd2e0
Evan Martin <martine@danga.com>
2008-10-15 23:50:59 -0700

    document Object

--- Object.hs
+++ Object.hs
@@ -1,4 +1,13 @@
-module Object where
+-- | A git \"object\" is a hash-addressed structure stored in the
+-- object store.
+
+module Object (
+    ObjectType(..)
+  , RawObject
+  , Object(..)
+  , Tree(..)
+  , objectTypeFromString
+) where
 
 import qualified Data.ByteString.Lazy as BL
 import Control.Monad.Error
@@ -6,18 +15,28 @@ import Control.Monad.Error
 import FileMode
 import Shared
 
-data ObjectType = TypeCommit | TypeTree | TypeBlob | TypeTag
-                  deriving (Eq, Show)
+-- | ObjectType represents the types of objects available.
+data ObjectType = TypeCommit  -- ^ tree and metadata about the tree
+                | TypeTree    -- ^ list of hashes and filenames
+                | TypeBlob    -- ^ collection of bytes (a file)
+                | TypeTag
+                deriving (Eq, Show)
+
+-- | RawObject is an object type and unparsed bytes of the object.
 type RawObject = (ObjectType, BL.ByteString)  -- Type, data.
-data Object = Blob BL.ByteString
+
+-- | Object represents a parsed object.
+data Object = Blob BL.ByteString  -- ^ blob of bytes (a file's contents)
             | Commit [(String,String)] String
+              -- ^ (key,value) metadata and commit message
             | ObTree Tree
             deriving Show
+
 data Tree = Tree [(GitFileMode, FilePath, Hash)]
             deriving Show
 
--- |Convert a String to an ObjectType.
--- These string types appear in "loose" objects.
+-- | Convert a String to an ObjectType.
+-- These string names appear in "loose" objects.
 objectTypeFromString :: String -> Either String ObjectType
 objectTypeFromString "blob"   = return TypeBlob
 objectTypeFromString "tree"   = return TypeTree
