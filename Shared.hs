56eed260eed9148a5db4069952b266ac90c04ac2
Evan Martin <martine@danga.com>
2008-10-16 00:21:09 -0700

    document Shared

--- Shared.hs
+++ Shared.hs
@@ -1,4 +1,15 @@
-module Shared where
+-- | Some shared utilites.
+module Shared (
+    breakAround
+  , firstTrue
+  , IOE, returnE
+  , asHex
+  , Hash(..), hashAsHex
+  , isHashString
+  , fromHex
+  , splitMSB
+  , strictifyBS
+) where
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
@@ -8,26 +19,35 @@ import Data.Char
 import Data.Word
 import Text.Printf
 
+-- | IOE is an Error monad wrapped around IO.  It's used for IO operations
+-- that may produce errors (represented as @Left String@).
 type IOE a = ErrorT String IO a
+-- | Lift a plain error (@Either String a@) into IOE.
 returnE :: Either String a -> IOE a
 returnE = ErrorT . return
 
+-- | A SHA-1 hash.
 newtype Hash = Hash B.ByteString deriving Eq
 
 instance Show Hash where
   show (Hash bs) = "[Hash " ++ asHex bs ++ "]"
 
+-- | Dump a ByteString as a String of hexidecimal characters.
 asHex :: B.ByteString -> String
 asHex = concatMap hex . B.unpack where
   hex c = printf "%02x" (c :: Word8)
+-- | Parse a string of hex into a ByteString.
 fromHex :: String -> B.ByteString
 fromHex = B.pack . bytes where
   bytes (x1:x2:rest) = parseHex x1 x2 : bytes rest
   bytes []           = []
   parseHex x1 x2 = fromIntegral (digitToInt x1 * 16 + digitToInt x2)
+
+-- | A hash as a hex string.
 hashAsHex :: Hash -> String
 hashAsHex (Hash bs) = asHex bs
 
+-- | Test whether a string looks like a hash (40 chars long, all hex).
 isHashString :: String -> Bool
 isHashString str = length str == 40 && all isHexDigit str
 
