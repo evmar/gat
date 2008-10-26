73c5a7285826cff4b9d6849d39ac01d50c816b8c
Evan Martin <martine@danga.com>
2008-10-26 14:37:25 -0700

    clean up parent finding in rev parsing

--- RevParse.hs
+++ RevParse.hs
@@ -47,26 +47,40 @@ p_ref = do rev <- (p_sha1 <|> p_symref)
     -- XXX what chars are allowed in a symref?
     p_symref = many1 (alphaNum <|> char '/') >>= return . RevSymRef
     -- Mods alter a Rev.
-    p_parent rev = do char '^'; return $ RevParent 1 rev
+    p_parent rev = do
+      char '^'
+      num <- option 1 $ liftM read (many1 digit)
+      return $ RevParent num rev
     p_ancestor rev = do
       char '~'
       num <- many1 digit
       return $ RevAncestor (read num) rev
 
+getParent :: Int -> Hash -> GitM Hash
+getParent nth hash = do
+  -- TODO: obey the "nth".
+  obj <- getObject hash
+  case obj of
+    ObCommit commit -> do
+      let hashstr = "commit " ++ hashAsHex hash
+      case commit_parents commit of
+        [] ->
+          fail $ hashstr ++ " has no parent"
+        parents | length parents < nth ->
+          fail $ hashstr ++ " has no " ++ show nth ++ "th parent"
+        parents ->
+         return $ Hash (fromHex (parents !! (nth-1)))
+    _ -> fail "object is not a commit"
 
 -- |Resolve a Rev into a Hash.
 resolve :: Rev -> GitM Hash
 resolve (RevHash hex) = return $ Hash (fromHex hex)
-resolve (RevParent nth rev) = do
-  hash <- resolve rev
-  -- TODO: obey the "nth".
-  obj <- getObject hash
-  case obj of
-    ObCommit commit ->
-      case commit_parents commit of
-        (hex:_) -> return $ Hash (fromHex hex)
-        _ -> fail "commit has no parent"
-    _ -> fail "object is not a commit"
+resolve (RevParent nth rev) = resolve rev >>= getParent nth
+resolve (RevAncestor nth rev) = resolve rev >>= go nth where
+  go 0 hash = return hash
+  go n hash = do
+    parent <- getParent 1 hash
+    go (n-1) parent
 resolve (RevSymRef name) = do
   fullname <- liftIO $ fullNameRef name
   case fullname of
