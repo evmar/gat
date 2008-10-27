d102806feb87e04fc01d54c261f143aab25a02e5
Evan Martin <martine@danga.com>
2008-10-26 17:45:21 -0700

    doc color

--- Color.hs
+++ Color.hs
@@ -1,7 +1,13 @@
-module Color where
+-- | Basic ANSI color handling.
+module Color (
+    Color(..)
+  , coloredLine
+) where
 
+-- | Just the colors that are actually used by Gat.
 data Color = Bold | Green | Red | Cyan | Yellow
 
+-- | Map a Color to the ANSI color code used to generate it.
 ansiColorCode :: Color -> String
 ansiColorCode Bold   = "1"
 ansiColorCode Red    = "31"
@@ -12,6 +18,7 @@ ansiColorCode Cyan   = "36"
 ansiEscape :: String -> String
 ansiEscape code = "\x1b[" ++ code ++ "m"
 
+-- | Wrap a string with a color + color reset pair.
 coloredLine :: Color -> String -> String
 coloredLine color str =
   ansiEscape (ansiColorCode color) ++ str ++ ansiEscape ""
